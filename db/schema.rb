021b26b22a02daed49ac15a3dfd3a1bab22b8ce2
Lorena Bento <lorenadgb@gmail.com>
2018-07-16 00:31:55 +1000

    Add screen model

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180715111526) do
+ActiveRecord::Schema.define(version: 20180715141413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
+  create_table "screens", force: :cascade do |t|
+    t.string "name"
+    t.integer "interval"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+    t.index ["user_id"], name: "index_screens_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,4 +41,5 @@ ActiveRecord::Schema.define(version: 20180715111526) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "screens", "users"
 end
